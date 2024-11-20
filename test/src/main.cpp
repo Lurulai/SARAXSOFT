525fd8477b85702f054c2c2866e70806447ae778
Argatmj <118488616+Argatmj@users.noreply.github.com>
2024-11-20 10:44:51 +0100

    Correct test file

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -2,24 +2,16 @@
 
 void setup() {
     Serial.begin(9600);
-    int arr[8] = {2, 3, 4, 5, 6, 7, 8, 9};
-
-    // Set each pin as input with the internal pull-up resistor enabled
-    for (int id : arr) {
-        pinMode(id, INPUT_PULLUP);
-    }
 }
 
 void loop() {
-    int arr[8] = {2, 3, 4, 5, 6, 7, 8, 9};
-    for (int id : arr) {
-        // Check if the pin is connected to GND (reads LOW)
-        if (digitalRead(id) == LOW) {
-            //Serial.print("Pin ");
-            Serial.println(id);
-            //Serial.println(" is connected to GND");
-        }
-
-        delay(100);
-    }
+   digitalWrite(2,HIGH);
+   int arr[8] = {4,5,6,7,8,9,10,11};
+   for(int id : arr){
+      int check = digitalRead(id);
+      if(check){
+         Serial.println(id);
+      }
+   }
+   delay(3000);
 }
